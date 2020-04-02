ffa9e72bfe70e8f998362892c42c58dd7722c6d9
Michael <maicol.asus@gmail.com>
2020-04-02 13:14:46 +0000

    [test] no remove()

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -63,7 +63,3 @@ class DBStorage():
 
         Session = sessionmaker(bind=self.__engine, expire_on_commit=False)
         self.__session = scoped_session(Session)
-
-    def close(self):
-        """ call remove() """
-        self.__session.remove()
