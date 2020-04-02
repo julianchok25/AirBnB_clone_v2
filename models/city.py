332a7163c4408cf559aa451421e04ca748839903
Julian Villegas <julian.villegasplus@gmail.com>
2020-04-02 19:50:39 +0000

    [FIX]city: add relationship to places and fix pep8

--- models/city.py
+++ models/city.py
@@ -2,6 +2,7 @@
 """This is the city class"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
@@ -13,3 +14,6 @@ class City(BaseModel, Base):
     __tablename__ = 'cities'
     state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
     name = Column(String(128), nullable=False)
+    # One2many relationship to places
+    places = relationship("Place", backref="cities",
+                          cascade="all, delete, delete-orphan")
