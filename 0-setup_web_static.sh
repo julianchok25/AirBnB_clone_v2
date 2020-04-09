d8a463b5e68043fff822622d9cff69745fe28b44
Michael <maicol.asus@gmail.com>
2020-04-09 19:28:37 +0000

    [add] add task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -4,14 +4,14 @@ sudo apt-get update -y
 sudo apt-get install nginx -y
 sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
-sudo echo "\
+echo "\
 <html>
   <head>
   </head>
   <body>
     Holberton School
   </body>
-</html>" > /data/web_static/releases/test/index.html
+</html>" > sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 sudo sed -i "38i location /hbnb_static {\nalias /data/web_static/current;\n}" /etc/nginx/sites-available/default
